f4aad79f40e192dd4e2b3a9bf81329880a79d3df
ArnulfoJimenez <87824514+ArnulfoJimenez@users.noreply.github.com>
2021-09-09 12:13:09 -0600

    Parcial

---
+++ ArnufoJosueSantosJimenez/ArnufoJosueSantosJimenez/Controllers/MontoController.cs
@@ -0,0 +1,38 @@
+﻿using ArnufoJosueSantosJimenez.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArnufoJosueSantosJimenez.Controllers
+{
+    public class MontoController : Controller
+    {
+        // GET: Monto
+        public ActionResult DescripcionMontoViewModel(MontoViewModel monto)
+        {
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View("MontoViewModel", monto );
+                }
+                return Redirect("MontoViewModel");
+            }
+        }
+        public ActionResult Error()
+        {
+            return View();
+        }
+            public ActionResult Operacion()
+            {
+            
+                return View();
+            }
+
+            public ActionResult MontoViewModel()
+            {
+                return View();
+            }
+        }
+    }
---
+++ ArnufoJosueSantosJimenez/ArnufoJosueSantosJimenez/Models/ViewModels/MontoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ArnufoJosueSantosJimenez.Models.ViewModels
+{
+    public class MontoViewModel
+    {
+
+        [Display(Name = "Cantidad")]
+        [Range(0, int.MaxValue, ErrorMessage = "Campo requirido")]
+        public int Cantidad { get; set; }
+    }
+}
